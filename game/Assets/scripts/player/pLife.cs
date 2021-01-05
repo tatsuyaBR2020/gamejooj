7e14955f60b70b23b63b3c8656ecb4f2c201c9c8
jose <gameflarexd@gamil.com>
2021-01-05 12:31:42 -0300

    anim

---
+++ game/Assets/scripts/player/pLife.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pLife : MonoBehaviour
+{
+    public Transform lifeinicial;
+    public GameObject vida;
+    public int vidas = 1;
+    public float Distancia;
+    public List<float> x = new List<float>();
+
+    private void Start()
+    {
+        reloadLifes();
+    }
+
+    public void reloadLifes()
+    {
+        x = new List<float>(vidas);
+        for(int i = 0;i < vidas; i++)
+        {
+            if(i == 0)
+            {
+                var obj = Instantiate(vida, lifeinicial.position, lifeinicial.rotation);
+                x[0] = obj.transform.position.x;
+                Debug.Log(x[0]);
+            }
+            if(i > 0)
+            {
+                var obj = Instantiate(vida,new Vector3(x[i - 1] +Distancia, 0, 0), transform.rotation);
+                x[i] = obj.transform.position.x;
+            }
+        }
+    }
+}
