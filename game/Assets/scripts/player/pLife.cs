c8b5efb52dae83a9d558b5d54aa6fb9e24904659
jose <gameflarexd@gamil.com>
2021-01-05 13:44:32 -0300

    alguma coisa

--- game/Assets/scripts/player/pLife.cs
+++ game/Assets/scripts/player/pLife.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class pLife : MonoBehaviour
 {
+    public Transform point;
     public GameObject vida;
     public int vidas = 1;
     public float Distancia;
@@ -11,7 +12,10 @@ public class pLife : MonoBehaviour
 
     private void Start()
     {
-        AddVida();
+        for(int i = 0;i < vidas; i++)
+        {
+            AddVida();
+        }
     }
 
     private void Update()
@@ -20,14 +24,34 @@ public class pLife : MonoBehaviour
         {
             AddVida();
         }
+        if (Input.GetKeyDown("t"))
+        {
+            Remove();
+        }
     }
-
     public void AddVida()
     {
         x.Add(vida);
-        int i = x.Count - 1;
-        x[i].transform.position = new Vector3(x[i].transform.position.x + Distancia,x[0].transform.position.y,0);
-        Instantiate(x[i],x[i].transform.position,transform.rotation);
+        int i = x.Count - 2;
+        if(x.Count > 1)
+        {
+            x[x.Count - 1].transform.position = new Vector3(x[i].transform.position.x + Distancia, x[0].transform.position.y, 0);
+            x[x.Count - 1] = Instantiate(vida, x[x.Count - 1].transform.position, transform.rotation);
+            x[x.Count - 1].transform.SetParent(point);
+        }
+        if(x.Count == 1)
+        {
+            x[0] = Instantiate(vida, point.position, transform.rotation);
+            x[0].transform.position = new Vector3(point.position.x, point.transform.position.y, 0);
+            x[0].transform.SetParent(point);
+        }
         Debug.Log("esta Funcionando");
+    }
+
+    public void Remove()
+    {
+        int i = x.Count - 1;
+        Destroy(x[i]);
+        x.Remove(x[i]);
     }
 }
